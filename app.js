83bf345c2ccb356dcea04ca7f090b12efe780d52
mazmulj <55931553+mazmulj@users.noreply.github.com>
2020-07-03 02:57:58 -0230

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+// Defining UI variables
+const form = document.querySelector('#task-form');
+const taskInput = document.querySelector('#task');
+const taskList = document.querySelector('.collection');
+const filter = document.querySelector('#filter');
+const clearBtn = document.querySelector('.clear-tasks');
