abefd5264c89f3ff1fdd658796cb57b34f3006b0
mazmulj <55931553+mazmulj@users.noreply.github.com>
2020-07-03 04:39:37 -0230

    Add files via upload

--- app.js
+++ app.js
@@ -4,3 +4,36 @@ const taskInput = document.querySelector('#task');
 const taskList = document.querySelector('.collection');
 const filter = document.querySelector('#filter');
 const clearBtn = document.querySelector('.clear-tasks');
+
+// Function to load all all event listeners
+loadEventListeners();
+
+function loadEventListeners() {
+    // Add task
+    form.addEventListener('submit', addTask);
+}
+
+// Add Task
+function addTask(e) {
+    if (taskInput.value === '') {
+        alert('Please add task');
+    }
+
+    // Add task to list
+    const li = document.createElement('li');
+    li.className = 'collection-item';
+    li.appendChild(document.createTextNode(taskInput.value));
+
+    // Create a link element
+    const link = document.createElement('a');
+    link.className = 'delete-item secondary-content';
+    link.innerHTML = '<i class="fa fa-remove"></i>';
+    li.appendChild(link); 
+
+    // Add li to tasklist
+    taskList.appendChild(li);
+    taskInput.value = '';
+
+    e.preventDefault();
+}
+
